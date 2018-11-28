e277e86b96aae250907e474fe0ab0a5ebbac8d28
sp-javier-barranco <javier.barranco@socialpoint.es>
2018-11-28 23:19:51 +0100

    Camera default + lateral SavePoint

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
+    struct CameraConfig
+    {
+        public Vector3 offset;
+        public Quaternion rotation;
+
+        public CameraConfig(Vector3 o, Quaternion q)
+        {
+            offset = o;
+            rotation = q;
+        }
+    }
+
     private GameObject _playerObj;
-    private Vector3 _offset;
+    private CameraConfig _defaultConfig;
+    private CameraConfig _lateralConfig;
+    private CameraConfig _activeCameraConfig;
+
     bool _autoCenter = true;
 
 	void Start () {
         _playerObj = GameObject.Find("Player");
-        _offset = transform.position - _playerObj.transform.position;
-	}
+
+        _defaultConfig = new CameraConfig(transform.position - _playerObj.transform.position, transform.rotation);
+
+        Vector3 lateralPosition = new Vector3(30.0f, 0.0f, 0.0f);
+        Quaternion lateralRotation = Quaternion.Euler(0.0f, -90.0f, 90.0f);
+        _lateralConfig = new CameraConfig(lateralPosition - _playerObj.transform.position, lateralRotation);
+
+        _activeCameraConfig = _defaultConfig;
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         if(_autoCenter)
         {
-            transform.position = _offset + _playerObj.transform.position;
+            transform.position = _activeCameraConfig.offset + _playerObj.transform.position;
         }
 	}
 
@@ -27,7 +50,7 @@ public class CameraMovement : MonoBehaviour {
         _autoCenter = false;
 
         float duration = 2.0f;
-        var dest = _offset + obj.transform.position;
+        var dest = _activeCameraConfig.offset + obj.transform.position;
         iTween.MoveTo(gameObject, dest, duration);
 
         yield return new WaitForSeconds(duration);
@@ -36,11 +59,23 @@ public class CameraMovement : MonoBehaviour {
     public IEnumerator centerToPlayer()
     {
         float duration = 1.0f;
-        var dest = _offset + _playerObj.transform.position;
+        var dest = _activeCameraConfig.offset + _playerObj.transform.position;
         iTween.MoveTo(gameObject, dest, duration);
 
         yield return new WaitForSeconds(duration);
 
         _autoCenter = true;
+    }
+
+    public void setLateralCamera()
+    {
+        _activeCameraConfig = _lateralConfig;
+        transform.rotation = _lateralConfig.rotation;
+    }
+
+    public void setDefaultCamera()
+    {
+        _activeCameraConfig = _defaultConfig;
+        transform.rotation = _defaultConfig.rotation;
     }
 }
---
+++ Assets/TriggerCameraChange.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCameraChange : MonoBehaviour {
+
+    public enum CameraOrientation
+    {
+        Default,
+        Lateral
+    }
+
+    public CameraOrientation _cameraOrientation;
+
+    private CameraMovement _cameraMovementScript;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _cameraMovementScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag != "Player")
+        {
+            return;
+        }
+
+        setCameraOrientation(_cameraOrientation);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            return;
+        }
+
+        setCameraOrientation(getOppositeCameraOrientation(_cameraOrientation));
+    }
+
+    private CameraOrientation getOppositeCameraOrientation(CameraOrientation co)
+    {
+        switch (co)
+        {
+            case CameraOrientation.Default: return CameraOrientation.Lateral;
+            case CameraOrientation.Lateral: return CameraOrientation.Default;
+        }
+
+        Debug.Assert(false, "Incorrect camera orientation");
+        return CameraOrientation.Default;
+    }
+
+    private void setCameraOrientation(CameraOrientation co)
+    {
+        switch (co)
+        {
+            case CameraOrientation.Default:
+                {
+                    _cameraMovementScript.setDefaultCamera();
+                    break;
+                }
+            case CameraOrientation.Lateral:
+                {
+                    _cameraMovementScript.setLateralCamera();
+                    break;
+                }
+        }
+    }
+}
